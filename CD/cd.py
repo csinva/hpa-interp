f6feb1b30d30ca3e9079c88f86309c632a4ac3eb
Ubuntu <ubuntu@ip-172-31-62-205.us-west-2.compute.internal>
2019-10-15 01:08:34 +0000

    updated cd score

---
+++ CD/cd.py
@@ -0,0 +1,212 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from copy import deepcopy
+import numpy as np
+from scipy.special import expit as sigmoid
+from cd_propagate import *
+
+
+def cd_densenet(blob, im_torch, model):
+    '''This implementation of cd is very long so that we can view CD at intermediate layers
+    In reality, one should use the loop contained in the above cd function
+    '''
+    # set up model
+    model.eval()
+
+    # set up blobs
+    blob = torch.cuda.FloatTensor(blob)
+    relevant = blob * im_torch
+    irrelevant = (1 - blob) * im_torch
+
+    mods = forward_mods(model)
+
+    scores = []
+    scores.append((relevant.clone(), irrelevant.clone()))
+
+    # propagate layers
+    with torch.no_grad():
+        # growth rate = 4; block config = (6, 12, 24, 16)
+        # normalization step
+        mean = [0.074598, 0.050630, 0.050891, 0.076287]#rgby
+        std =  [0.122813, 0.085745, 0.129882, 0.119411]
+        bias = torch.zeros(irrelevant.size()).to('cuda')
+        rel = torch.zeros(irrelevant.size()).to('cuda')
+        irrel = torch.zeros(irrelevant.size()).to('cuda')
+        for i in range(model.module.in_channels):
+            bias[:,i,:,:] = (bias[:,i,:,:] - mean[i]) / std[i]
+            rel[:,i,:,:] = relevant[:,i,:,:] / std[i]
+            irrel[:,i,:,:] = irrelevant[:,i,:,:] / std[i]
+        prop_rel = torch.abs(rel)
+        prop_irrel = torch.abs(irrel)
+        prop_sum = prop_rel + prop_irrel
+        prop_rel = torch.div(prop_rel, prop_sum)
+        prop_irrel = torch.div(prop_irrel, prop_sum)
+        relevant, irrelevant = rel + torch.mul(prop_rel, bias), irrel + torch.mul(prop_irrel, bias)
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # initial convolution
+        relevant, irrelevant = propagate_init_conv(relevant, irrelevant, mods[0])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        if model.module.large:
+            relevant, irrelevant = propagate_pooling(relevant, irrelevant, mods[1])
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+        # denseblock1
+        relevant, irrelevant = propagate_denseblock(relevant, irrelevant, mods[2])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # transition1
+        relevant, irrelevant = propagate_transition(relevant, irrelevant, mods[3])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # denseblock2
+        relevant, irrelevant = propagate_denseblock(relevant, irrelevant, mods[4])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # transition2
+        relevant, irrelevant = propagate_transition(relevant, irrelevant, mods[5])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # denseblock3
+        relevant, irrelevant = propagate_denseblock(relevant, irrelevant, mods[6])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # transition3
+        relevant, irrelevant = propagate_transition(relevant, irrelevant, mods[7])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # denseblock4
+        relevant, irrelevant = propagate_denseblock(relevant, irrelevant, mods[8])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # final batch norm
+        relevant, irrelevant = propagate_batchnorm2d(relevant, irrelevant, mods[9])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        # ReLU layer
+        relevant, irrelevant = propagate_relu(relevant, irrelevant, mods[10])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+        if model.module.dropout:
+            # adaptive avgpooling and maxpooling
+            rel0, irrel0 = propagate_avgpooling(relevant, irrelevant, mods[11])
+            rel1, irrel1 = propagate_pooling(relevant, irrelevant, mods[12])
+            relevant, irrelevant = torch.cat((rel0, rel1), dim=1), torch.cat((irrel0, irrel1), dim=1)
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+            # reshape
+            relevant = relevant.view(relevant.size(0), -1)
+            irrelevant = irrelevant.view(irrelevant.size(0), -1)
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+            # bn1 layer
+            relevant, irrelevant = propagate_batchnorm2d(relevant, irrelevant, mods[13])
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+            # fn1 layer
+            relevant, irrelevant = propagate_conv_linear(relevant, irrelevant, mods[14])
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+            # ReLU layer
+            relevant, irrelevant = propagate_relu(relevant, irrelevant, mods[15])
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+            # bn2 layer
+            relevant, irrelevant = propagate_batchnorm2d(relevant, irrelevant, mods[16])
+            scores.append((relevant.clone(), irrelevant.clone()))
+
+        # reshape
+        relevant = relevant.view(relevant.size(0), -1)
+        irrelevant = irrelevant.view(irrelevant.size(0), -1)
+
+        # linear layer
+        relevant, irrelevant = propagate_conv_linear(relevant, irrelevant, mods[17])
+        scores.append((relevant.clone(), irrelevant.clone()))
+
+    return relevant, irrelevant, scores
+
+
+def forward_mods(model):
+    # set up model
+    model.eval()
+
+    model_mods = list(model.modules())
+    mods = []
+
+    append_mod(model_mods[435], model.module.conv1, mods)
+    if model.module.large:
+        append_mod(False, model.module.maxpool, mods)
+    append_mod(model_mods[436][0], model.module.encoder2[0], mods)
+    append_mod(model_mods[437][0], model.module.encoder3[0], mods)
+    append_mod(model_mods[437][1], model.module.encoder3[1], mods)
+    append_mod(model_mods[438][0], model.module.encoder4[0], mods)
+    append_mod(model_mods[438][1], model.module.encoder4[1], mods)
+    append_mod(model_mods[439][0], model.module.encoder5[0], mods)
+    append_mod(model_mods[439][1], model.module.encoder5[1], mods)
+    append_mod(model_mods[439][2], model.module.encoder5[2], mods)
+    append_mod(False, nn.ReLU(inplace=True), mods)
+    if model.module.dropout:
+        append_mod(False, nn.AdaptiveAvgPool2d(1), mods)
+        append_mod(False, nn.AdaptiveMaxPool2d(1), mods)
+        append_mod(model_mods[443], model.module.bn1, mods)
+    #     append_mod(False, F.dropout(x, p=0.5, training=self.training), mods)
+        append_mod(model_mods[444], model.module.fc1, mods)
+        append_mod(False, nn.ReLU(inplace=True), mods)
+        append_mod(model_mods[445], model.module.bn2, mods)
+    #     append_mod(False, F.dropout(x, p=0.5, training=self.training), mods)
+    else:
+        append_mod(False, nn.AdaptiveAvgPool2d(1), mods)
+    append_mod(model_mods[442], model.module.logit, mods)
+    return mods
+
+
+def append_mod(net_mod, mod, list_of_modules):
+    if net_mod == False:
+        list_of_modules.append(mod)
+    else:
+        Err = net_mod != mod
+        if not Err:
+            list_of_modules.append(net_mod)
+        else:
+            print('Error')
+            raise ValueError
+
+
+def forward_pass(im_torch, model):
+    # set up model
+    model.eval()
+    mods = forward_mods(model)
+
+    outputs = []
+    output = im_torch.new_tensor(im_torch)
+    outputs.append(output.clone())
+
+    mean = [0.074598, 0.050630, 0.050891, 0.076287]#rgby
+    std =  [0.122813, 0.085745, 0.129882, 0.119411]
+    for i in range(model.module.in_channels):
+        output[:,i,:,:] = (output[:,i,:,:] - mean[i]) / std[i]
+    outputs.append(output.clone())
+    # propagate layers
+    with torch.no_grad():
+        for i, mod in enumerate(mods[:11]):
+            output = mod(output)
+            outputs.append(output.clone())
+
+        output0 = mods[11](output)
+        output1 = mods[12](output)
+        output = torch.cat((output0, output1), dim=1)
+        outputs.append(output.clone())
+
+        output = output.view(output.size(0), -1)
+        outputs.append(output.clone())
+
+        for i, mod in enumerate(mods[13:-1]):
+            output = mod(output)
+            outputs.append(output.clone())
+
+        output = output.view(output.size(0), -1)
+        output = mods[17](output)
+        outputs.append(output.clone())
+    return output, outputs
---
+++ CD/cd_propagate.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn.functional as F
+from copy import deepcopy
+import numpy as np
+from scipy.special import expit as sigmoid
+
+
+# propagate a three-part
+def propagate_three(a, b, c, activation):
+    a_contrib = 0.5 * (activation(a + c) - activation(c) + activation(a + b + c) - activation(b + c))
+    b_contrib = 0.5 * (activation(b + c) - activation(c) + activation(a + b + c) - activation(a + c))
+    return a_contrib, b_contrib, activation(c)
+
+
+# propagate tanh nonlinearity
+def propagate_tanh_two(a, b):
+    return 0.5 * (np.tanh(a) + (np.tanh(a + b) - np.tanh(b))), 0.5 * (np.tanh(b) + (np.tanh(a + b) - np.tanh(a)))
+
+
+# propagate convolutional or linear layer
+def propagate_conv_linear(relevant, irrelevant, module, device='cuda'):
+    bias = module(torch.zeros(irrelevant.size()).to(device))
+    rel = module(relevant) - bias
+    irrel = module(irrelevant) - bias
+
+    # elementwise proportional
+    prop_rel = torch.abs(rel)
+    prop_irrel = torch.abs(irrel)
+    prop_sum = prop_rel + prop_irrel
+    # if prop_sum is zero set the fraction to be 1/2
+    ind = prop_sum==0
+    prop_rel[ind] = 1.
+    prop_irrel[ind] = 1.
+    prop_sum[ind] = 2.
+    prop_rel = torch.div(prop_rel, prop_sum)
+    prop_irrel = torch.div(prop_irrel, prop_sum)
+    return rel + torch.mul(prop_rel, bias), irrel + torch.mul(prop_irrel, bias)
+
+
+# propagate ReLu nonlinearity
+def propagate_relu(relevant, irrelevant, activation, device='cuda'):
+    swap_inplace = False
+    try:  # handles inplace
+        if activation.inplace:
+            swap_inplace = True
+            activation.inplace = False
+    except:
+        pass
+    zeros = torch.zeros(relevant.size()).to(device)
+    rel_score = activation(relevant)
+    irrel_score = activation(relevant + irrelevant) - activation(relevant)
+    if swap_inplace:
+        activation.inplace = True
+    return rel_score, irrel_score
+
+
+# propagate maxpooling operation
+### for padding???
+def propagate_pooling(relevant, irrelevant, pooler):
+    # get both indices
+    p = deepcopy(pooler)
+    p.return_indices = True
+    both, both_ind = p(relevant + irrelevant)
+    rel, irrel = unpool(relevant, both_ind), unpool(irrelevant, both_ind)
+    return rel, irrel
+
+
+# propagate dropout operation
+def propagate_dropout(relevant, irrelevant, dropout):
+    return dropout(relevant), dropout(irrelevant)
+
+
+# propagate avgpooling operation
+def propagate_avgpooling(relevant, irrelevant, pooler):
+    return pooler(relevant), pooler(irrelevant)
+
+
+# propagate batchnorm operation
+### can we do as conv/linear layers
+def propagate_batchnorm2d(relevant, irrelevant, module, device='cuda'):
+    bias = module(torch.zeros(irrelevant.size()).to(device))
+    rel = module(relevant) - bias
+    irrel = module(irrelevant) - bias
+
+    # elementwise proportional
+    prop_rel = torch.abs(rel)
+    prop_irrel = torch.abs(irrel)
+    prop_sum = prop_rel + prop_irrel
+    # if prop_sum is zero set the fraction to be 1/2
+    ind = prop_sum==0
+    prop_rel[ind] = 1.
+    prop_irrel[ind] = 1.
+    prop_sum[ind] = 2.
+    prop_rel = torch.div(prop_rel, prop_sum)
+    prop_irrel = torch.div(prop_irrel, prop_sum)
+    return rel + torch.mul(prop_rel, bias), irrel + torch.mul(prop_irrel, bias)
+
+
+# propagate initial convolution operation
+def propagate_init_conv(relevant, irrelevant, init_modules, device='cuda'):
+    # (conv0): Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    # (norm0): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+    # (relu0): ReLU(inplace)
+    # (pool0): MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+    relevant, irrelevant = propagate_conv_linear(relevant, irrelevant, init_modules[0])
+    relevant, irrelevant = propagate_batchnorm2d(relevant, irrelevant, init_modules[1])
+    relevant, irrelevant = propagate_relu(relevant, irrelevant, init_modules[2])
+    relevant, irrelevant = propagate_pooling(relevant, irrelevant, init_modules[3])
+    return relevant, irrelevant
+
+
+# propagate denseblock
+def propagate_denseblock(relevant, irrelevant, block_modules, device='cuda'):
+    num_layers = len(block_modules)
+    for i in range(num_layers):
+        relevant, irrelevant = propagate_denselayer(relevant, irrelevant, block_modules[i], device)
+    return relevant, irrelevant
+
+
+# propagate transition operation
+def propagate_transition(relevant, irrelevant, trans_modules, device='cuda'):
+    # (norm): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+    # (relu): ReLU(inplace)
+    # (conv): Conv2d(256, 128, kernel_size=(1, 1), stride=(1, 1), bias=False)
+    # (pool): AvgPool2d(kernel_size=2, stride=2, padding=0)
+    relevant, irrelevant = propagate_batchnorm2d(relevant, irrelevant, trans_modules[0])
+    relevant, irrelevant = propagate_relu(relevant, irrelevant, trans_modules[1])
+    relevant, irrelevant = propagate_conv_linear(relevant, irrelevant, trans_modules[2])
+    relevant, irrelevant = propagate_avgpooling(relevant, irrelevant, trans_modules[3])
+    return relevant, irrelevant
+
+
+# propagate denselayer
+def propagate_denselayer(relevant, irrelevant, modules, device='cuda'):
+    rel, irrel = relevant.new_tensor(relevant).to(device), irrelevant.new_tensor(irrelevant).to(device)
+
+    # propagate dense layers
+    rel, irrel = propagate_batchnorm2d(rel, irrel, modules[0])
+    rel, irrel = propagate_relu(rel, irrel, modules[1])
+    rel, irrel = propagate_conv_linear(rel, irrel, modules[2])
+    rel, irrel = propagate_batchnorm2d(rel, irrel, modules[3])
+    rel, irrel = propagate_relu(rel, irrel, modules[4])
+    rel, irrel = propagate_conv_linear(rel, irrel, modules[5])
+    return torch.cat([relevant, rel], 1), torch.cat([irrelevant, irrel], 1)
+
+
+# pooling function
+def unpool(tensor, indices):
+    batch_size, in_channels = indices.size()[:2]
+    output = torch.ones_like(indices, dtype=torch.float)
+    for i in range(batch_size):
+        for j in range(in_channels):
+            output[i,j] = tensor[i,j].view(-1)[indices[i,j].view(-1)].view(indices.size()[2:])
+    return output
